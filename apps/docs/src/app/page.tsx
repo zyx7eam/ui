218e82f5622bf46774b8d0f62bd56f0dbb87e381
Hamude shahin <76263045+hamudeshahin@users.noreply.github.com>
2023-10-16 20:09:29 +0300

    Simple home page & deployment

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,7 +1,22 @@
+import { MoveRightIcon } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from 'ui';
+
 export default function Home() {
   return (
     <main className='relative z-50'>
-      <h1>Home Page</h1>
+      <div className='mx-auto max-w-6xl '>
+        <div className='flex translate-y-64 transform flex-col gap-2 pb-36'>
+          <h2 className='mx-auto my-10 max-w-3xl text-center text-4xl font-bold md:text-7xl'>
+            New React UI Component Library!
+          </h2>
+          <Link href='/docs/components' className='mx-auto w-fit'>
+            <Button endContent={<MoveRightIcon size={18} />}>
+              Explore Components
+            </Button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
