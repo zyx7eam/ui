f97734e23cca0b740eb658e1390276da5a37f042
hamudeshahin <hamedsahin2018@icloud.com>
2024-01-02 22:54:25 +0300

    Flex Component Ready! - Docs Updates Started

--- apps/docs/src/components/blob.tsx
+++ apps/docs/src/components/blob.tsx
@@ -8,10 +8,10 @@ const Blob = () => {
   const pathname = usePathname();
 
   const classNames = cn(
-    'bg-primary transition-all fixed z-0 h-[436px] w-[436px] transform rounded-full opacity-20 blur-3xl',
+    'bg-primary transition-all fixed z-0 h-[768px] w-[768px] transform rounded-full opacity-20 blur-3xl',
     pathname.startsWith('/docs')
       ? 'right-2 bottom-0 w-80 h-80'
-      : 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2',
+      : 'left-1/2 -translate-x-1/2 top-0',
   );
   return <div className={classNames} />;
 };
